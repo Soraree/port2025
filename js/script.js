07eeb2a8ffc9e75df916846cf10689e8d84ed53e
Soraree <sswory@naver.com>
2025-05-07 00:03:34 +0900

    style: header에 scroll 애니메이션 추가

--- js/script.js
+++ js/script.js
@@ -1,23 +1,4 @@
 $(document).ready(function(){
     "use strict";
 
-    HeaderScrolled();
-
-    // 헤더 스크롤 스타일
-    function HeaderScrolled(){
-        $(window).on("scroll", function(){
-            if(window.scrollY > 90){
-                $("#header").addClass("scrolled");
-            }else{
-                $("#header").removeClass("scrolled");
-            }
-        });
-    }
-    
-
-
-
-
-
-
 });
